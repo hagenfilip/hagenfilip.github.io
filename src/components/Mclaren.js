f589ac3ad3932e9ebec543aa9f506286d077b7d7
Filip <91458751+hagenfilip@users.noreply.github.com>
2022-05-20 15:02:58 +0200

    remove all

--- src/components/Mclaren.js
+++
@@ -1,8 +0,0 @@
-import { useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import mclaren from "../models/mclaren.glb"
-
-export default function Mclaren(props){
-    const { scene } = useLoader(GLTFLoader, mclaren)
-    return <primitive object={scene} {...props}/>
-}
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import './styles.css'
-import ModelRenderer from './components/ModelRenderer'
-
-createRoot(document.getElementById('root')).render(
-    <>
-        <ModelRenderer />
-    </>
-    )
